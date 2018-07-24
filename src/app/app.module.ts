c182b0f4235921d28422943eb94a62d9d2863ad9
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-07-23 22:56:20 -0400

    components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { SharedComponent } from './shared/shared.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    SharedComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
